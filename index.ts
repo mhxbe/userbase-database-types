b8c1ea7b049e58199d2c06e36bca075f801c66d0
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-11-09 20:25:17 +0100

    Kickstart test

---
+++ index.ts
@@ -0,0 +1,55 @@
+import userbase from "userbase-js";
+// const userbase = require("userbase-js");
+
+const {
+    openDatabase,
+    getDatabases,
+    insertItem,
+    updateItem,
+    deleteItem,
+    putTransaction,
+    uploadFile,
+    getFile,
+    shareDatabase,
+    modifyDatabasePermissions
+} = userbase;
+
+openDatabase({
+    changeHandler: console.log
+});
+
+getDatabases({});
+
+insertItem({
+    item: {}
+});
+
+updateItem({
+    item: {},
+    itemId: "abc"
+});
+
+deleteItem({
+    itemId: "abc"
+});
+
+putTransaction({
+    operations: []
+});
+
+uploadFile({
+    itemId: "abc",
+    file: new File(["test" as BlobPart], "test.txt")
+});
+
+getFile({
+    fileId: "abc"
+});
+
+shareDatabase({
+    username: 'mhxbe'
+}) 
+
+modifyDatabasePermissions({
+    username: 'mhxbe'
+})
